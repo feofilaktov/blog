7b1ab4f06214728685b9ba5cab9641dcc8b14bbd
Vladimir Feofilaktov <vladimir@feofilaktov.com>
2020-07-19 00:17:07 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+# add DB type parameters
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# db constructor
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id  # id for arcticle identification
+
+
+# in python console
+# from app import db
+# db.create.all()
+# blog.app will be created
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()  # Take from DB(class articel) all data sorted by date
+    return render_template('posts.html', articles=articles)  # sending articles
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'При удалении статьи произошла ошибка'
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('create-article.html')
+
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При добавлении статьи произошла ошибка'
+    else:
+        return render_template('post_update.html', article=article)
+
+# Авторизация в будущем
+# @app.route('/login', methods=['POST', 'GET'])
+# def login_admin():
+#     username = request.form['login']
+#     userpass = request.form['password']
+#
+#     count_users = count_users_DB()
+#     for i in row:
+#         if username in i and userpass in i:
+#             return render_template('admin.html', username=username, userpass=userpass, row=row, count_users=count_users)
+#     else:
+#         return update_user()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
